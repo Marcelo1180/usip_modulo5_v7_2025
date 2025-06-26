4501e8c0f550de5430421ad2d5c9343bd02cb1d1
Marcelo Arteaga <arteagamarcelo@gmail.com>
2025-06-26 15:15:19 -0400

    dia 2

--- ecoapp/settings.py
+++ ecoapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'inventario',
 ]
 
 MIDDLEWARE = [
@@ -72,10 +73,20 @@ WSGI_APPLICATION = 'ecoapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "usip_modulo5_v7",
+        "USER": "postgres",
+        "PASSWORD": "Developer",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categoria, Producto
+
+admin.site.register(Categoria)
+
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'categoria', 'precio', 'unidades', 'disponible')
+    ordering = ('nombre',)
+    search_fields = ('nombre',)
+    list_filter = ('disponible',)
+
+admin.site.register(Producto, ProductoAdmin)
